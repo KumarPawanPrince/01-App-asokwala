8091642b9de6be7acd407777431dc8c0e7975ce6
JavaProject039am <javaproject.03.9am@gmail.com>
2020-05-28 06:47:02 +0530

    changes for 103

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		int i = 10;
 	}
 
 }
